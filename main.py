8c68c4b27d2d911ca2fca2ffe01e2dbc69e33168
phin 801 <phinease456@gmail.com>
2021-07-23 08:26:46 -0600

    Commit Changes (Test for Diff)

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ import pygame
 # it is better to have an extra variable, than an extremely long line.
 img_path = "./player.png"
 
-class Bird:  # represents the bird, not the game
+
+class Bird(pygame.sprite.Sprite):
 	def __init__(self):
-		""" The constructor of the class """
-		self.image = pygame.image.load(img_path)
-
-		# the bird's position
-
-		self.x = 50
-		self.y = 50
-		self.speed = 1.5
+		self.x = 0
+		self.y = 0
+		self.speed = 3
 		self.ckey = None
-		self.width, self.height = [self.image.get_width(), self.image.get_height()]
+		super().__init__()
+		self.imgdct = {}
+		self.images = self.imgdct["RIGHT"], self.imgdct["DOWN"], self.imgdct["UP"], self.imgdct["LEFT"] = [[
+				pygame.image.load(f"images/{itm}/player_{num}.png")
+				for num in range(0, 4)
+		] for itm in ["RIGHT", "DOWN", "UP", "LEFT"]]
+		self.index = 0
+		self.image = pygame.image.load("images/RIGHT/player_0.png")
+		self.slowdown = 6
+		self.actualslow = 0
 
-	def handle_keys(self,k=None):
-		""" Handles Keys """
+	def handle_keys(self, k=None):
 		if k == None:
 			key = pygame.key.get_pressed()
 		else:
@@ -27,48 +31,49 @@ class Bird:  # represents the bird, not the game
 			self.ckey = key
 		if self.ckey != None and self.ckey != key and 1 in tuple(key):
 			self.ckey = key
-		dist = 1 # distance moved in 1 frame, try changing it to 5
-		if key[pygame.K_DOWN] or k=="DOWN": # down key
-			self.y += dist * self.speed # move down
-		elif key[pygame.K_UP] or k=="UP": # up key
-			self.y -= dist * self.speed # move up
-		elif key[pygame.K_RIGHT] or k=="RIGHT": # right key
-			self.x += dist * self.speed # move right
-		elif key[pygame.K_LEFT] or k=="LEFT": # left key
-			self.x -= dist * self.speed # move left
+		dist = 1
+		if key[pygame.K_DOWN] or k == "DOWN":
+			self.y += dist * self.speed
+		elif key[pygame.K_UP] or k == "UP":
+			self.y -= dist * self.speed
+		elif key[pygame.K_RIGHT] or k == "RIGHT":
+			self.x += dist * self.speed
+		elif key[pygame.K_LEFT] or k == "LEFT":
+			self.x -= dist * self.speed
 
-	def draw(self, surface):
-		""" Draw on surface """
-		# blit yourself at your current position
-		self.width, self.height = [self.image.get_width(), self.image.get_height()]
+	def draw(self, surface, direction="RIGHT"):
+		if self.actualslow == self.slowdown:
+			self.actualslow = 0
+			self.index += 1
+			cont = True
+		else:
+			self.actualslow += 1
+			cont = False
+		if not cont:
+			surface.blit(self.image, (self.x, self.y))
+			return 0
+		if self.index >= len(self.imgdct[direction]):
+			self.index = 0
+		self.image = self.imgdct[direction][self.index]
+		self.index += 1
 		surface.blit(self.image, (self.x, self.y))
 
-pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
-screen.fill((255,255,255)) # fill the screen with white
-bird = Bird() # create an instance
-clock = pygame.time.Clock()
 
+pygame.init()
+screen = pygame.display.set_mode((640, 400))
+
+bird = Bird()
+clock = pygame.time.Clock()
+playergroup = pygame.sprite.Group(bird)
 running = True
-bckey=None
+bckey = None
 
 while running:
-
-	# handle every event since the last frame.
-	screen.fill((255, 255, 255))  # fill the screen with white
-	pygame.draw.rect(screen, (0,0,0), (5,5,990,990), 20)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			pygame.quit() # quit the screen
+			pygame.quit()
 			running = False
-	if pygame.Surface.get_at(screen, (round(bird.x-2),round(bird.y)))==(0,0,0):
-		bird.x = bird.x+3
-	if pygame.Surface.get_at(screen, (round(bird.x+bird.width),round(bird.y-2)))==(0,0,0):
-		bird.y = bird.y+3
-	if pygame.Surface.get_at(screen, (round(bird.x+bird.width+3),round(bird.y+bird.height-1)))==(0,0,0):
-		bird.x = bird.x-3
-	if pygame.Surface.get_at(screen, (round(bird.x),round(bird.y+bird.width+2)))==(0,0,0):
-		bird.y = bird.y-3
+
 	bird.handle_keys()
 	bckey = {
 		"DOWN": [False if bird.ckey[pygame.K_DOWN] == 0 else True][0],
@@ -76,12 +81,13 @@ while running:
 		"LEFT": [False if bird.ckey[pygame.K_LEFT] == 0 else True][0],
 		"RIGHT": [False if bird.ckey[pygame.K_RIGHT] == 0 else True][0],
 	}
+	kk = "RIGHT"
 	for x in list(bckey.keys()):
 		if bckey[x] == True:
 			bird.handle_keys(k=x)
+			kk = x
 			break
-
-	bird.draw(screen)# draw the bird to the screen
-	pygame.display.update() # update the screen
-
-	clock.tick(120)
+	screen.fill((0, 0, 0))
+	bird.draw(screen, direction=kk)
+	pygame.display.update()
+	clock.tick(40)
